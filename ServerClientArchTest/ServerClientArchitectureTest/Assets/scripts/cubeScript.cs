df97c0e188b10e7c414df4fc843f88bab0191feb
Manisha W <mwann031@uottawa.ca>
2018-03-04 16:51:26 -0500

    client/server basic app

---
+++ ServerClientArchTest/ServerClientArchitectureTest/Assets/scripts/cubeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Network.isServer)
+		{
+			Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			float speed = 5;
+			this.transform.Translate(speed * moveDir * Time.deltaTime);
+		}
+	}
+}
